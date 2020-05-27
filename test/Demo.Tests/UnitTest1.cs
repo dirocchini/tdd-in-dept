63c299a448daf20a7fdb44cf35d59f3ee0689dc7
dirocchini <roquinni@gmail.com>
2020-05-27 14:28:40 -0300

    first commit

---
+++ test/Demo.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
