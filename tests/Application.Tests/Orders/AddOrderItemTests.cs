47d5d8d1ec3103e5bd96c3b9d413b57e1c0a4bff
dirocchini <roquinni@gmail.com>
2020-06-15 20:10:17 -0300

    add order item command

---
+++ tests/Application.Tests/Orders/AddOrderItemTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sales.Application.Commands;
+using Xunit;
+
+namespace Application.Tests.Orders
+{
+    public class AddOrderItemTests
+    {
+        [Fact(DisplayName = "Add Order Item Valid Command")]
+        [Trait("Category", "Sales - Order Commands")]
+        public void AddOrderItemCommand_ValidCommand_ShouldBeValid()
+        {
+            // Arrange
+            var command = new AddOrderItemCommand(Guid.NewGuid(), Guid.NewGuid(), "order item x", 2, 100);
+
+            // Act
+            var result = command.IsValid();
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "Add Order Item Invalid Command")]
+        [Trait("Category", "Sales - Order Commands")]
+        public void AddOrderItemCommand_InvalidCommand_ShouldBeValid()
+        {
+            // Arrange
+            var command = new AddOrderItemCommand(Guid.Empty, Guid.Empty, "", 0, 0);
+
+            // Act
+            var result = command.IsValid();
+
+            // Assert
+            Assert.False(result);
+            Assert.Contains(AddOrderItemCommandValidation.IdClienteErroMsg, command.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AddOrderItemCommandValidation.IdProdutoErroMsg, command.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AddOrderItemCommandValidation.NomeErroMsg, command.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AddOrderItemCommandValidation.QtdMinErroMsg, command.ValidationResult.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(AddOrderItemCommandValidation.ValorErroMsg, command.ValidationResult.Errors.Select(c => c.ErrorMessage));
+
+        }
+    }
+}
