498c56d09efd39c80f1b8330c11a283516a719bc
dirocchini <roquinni@gmail.com>
2020-06-15 20:42:13 -0300

    initial

---
+++ tests/Application.Tests/Orders/OrderCommandHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sales.Application.Commands;
+using Xunit;
+
+namespace Application.Tests.Orders
+{
+    public class OrderCommandHandlerTests
+    {
+        [Fact(DisplayName = "Add New Order Item to New Order Successfully")]
+        [Trait("Category", "Sales - Order Command Handler")]
+        public void AddOrderItem_NewOrder_ShouldAddItem()
+        {
+            // Arrange
+            var command = new AddOrderItemCommand(new Guid(),new Guid(),"item x", 2, 100 );
+
+            // Act
+
+            // Assert
+                
+        }
+    }
+}
