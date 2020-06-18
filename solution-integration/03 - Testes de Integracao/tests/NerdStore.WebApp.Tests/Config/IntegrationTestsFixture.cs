0f87d68b2f83280258d01828cc6139aa0bb2c002
dirocchini <roquinni@gmail.com>
2020-06-18 19:47:02 -0300

    configuration

---
+++ solution-integration/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NerdStore.WebApp.MVC;
+using Xunit;
+
+namespace NerdStore.WebApp.Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>>
+    {
+    }
+
+
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>>
+    {
+    }
+
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly LojaAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
+        {
+            var clientOption = new WebApplicationFactoryClientOptions
+            {
+
+            };
+
+            Factory = new LojaAppFactory<TStartup>();
+            Client = Factory.CreateClient(options: clientOption);
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
---
+++ solution-integration/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTestes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper.Configuration;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class UsuarioTestes
+    {
+        private readonly IntegrationTestsFixture<StartupWebTests> _testsFixture;
+
+        public UsuarioTestes(IntegrationTestsFixture<StartupWebTests> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+    }
+}
