276ef6eb53e847c6f563b2e11d621bb3eb5445e4
dirocchini <roquinni@gmail.com>
2020-06-21 13:19:38 -0300

    testing user registration

--- solution-integration/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
+++ solution-integration/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
+using Bogus;
 using Microsoft.AspNetCore.Mvc.Testing;
 using NerdStore.WebApp.MVC;
 using Xunit;
@@ -20,8 +22,12 @@ namespace NerdStore.WebApp.Tests.Config
     }
 
 
-    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    public class  IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
     {
+        public string AntiForgeryFieldName = "__RequestVerificationToken";
+        public string UsuarioEmail = "__RequestVerificationToken";
+        public string UsuarioSenha= "__RequestVerificationToken";
+
         public readonly LojaAppFactory<TStartup> Factory;
         public HttpClient Client;
 
@@ -40,6 +46,24 @@ namespace NerdStore.WebApp.Tests.Config
         {
             Client.Dispose();
             Factory.Dispose();
+        }
+
+        public void GerarUserSenha()
+        {
+            var faker = new Faker("pt_BR");
+            UsuarioEmail = faker.Internet.Email().ToLower();
+            UsuarioSenha = faker.Internet.Password(8, false, "", "@1Ab_");
+        }
+
+        public string ObterAntiForgeryToken(string htmlBody)
+        {
+            var requestVerificationTokenMatch =
+                Regex.Match(htmlBody, $@"\<input name=""{AntiForgeryFieldName}"" type=""hidden"" value=""([^""]+)"" \/\>");
+
+            if (requestVerificationTokenMatch.Success)
+                return requestVerificationTokenMatch.Groups[1].Captures[0].Value;
+
+            throw new ArgumentException($"Anti forgery token '{AntiForgeryFieldName}' não encontrado no HTML", nameof(htmlBody));
         }
     }
 }
--- solution-integration/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTestes.cs
+++ solution-integration/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTestes.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Internal.Account;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
 using NerdStore.WebApp.MVC;
 using NerdStore.WebApp.Tests.Config;
+using Xunit;
 
 namespace NerdStore.WebApp.Tests
 {
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class UsuarioTestes
     {
         private readonly IntegrationTestsFixture<StartupWebTests> _testsFixture;
@@ -15,5 +22,45 @@ namespace NerdStore.WebApp.Tests
         {
             _testsFixture = testsFixture;
         }
+
+        [Fact(DisplayName = "Register User")]
+        [Trait("Category", "Web Integration - User")]
+        public async Task User_Register_ShouldRegisterUser()
+        {
+            // Arrange
+            _testsFixture.GerarUserSenha();
+            string email = _testsFixture.UsuarioEmail;
+            string pass = _testsFixture.UsuarioSenha;
+
+            var inicitalResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
+            inicitalResponse.EnsureSuccessStatusCode();
+
+            var antiForgeryToken = _testsFixture.ObterAntiForgeryToken(await inicitalResponse.Content.ReadAsStringAsync());
+
+            var formData = new Dictionary<string, string>
+            {
+                {_testsFixture.AntiForgeryFieldName, antiForgeryToken},
+                {"Input.Email", email},
+                {"Input.Password", pass},
+                {"Input.ConfirmPassword", pass}
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register")
+            {
+                Content = new FormUrlEncodedContent(formData)
+
+            };
+
+            // Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+            // Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains($"Hello {email}", responseString);
+        }
     }
 }
+
+
