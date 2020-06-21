b0d0e4cbc646aa17215f22b5df149d34ec597195
dirocchini <roquinni@gmail.com>
2020-06-21 14:08:29 -0300

    testing user login and added priority

--- solution-integration/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTestes.cs
+++ solution-integration/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTestes.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper.Configuration;
+using Features.Tests;
 using Microsoft.AspNetCore.Identity.UI.V3.Pages.Internal.Account;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
@@ -13,6 +14,7 @@ using Xunit;
 
 namespace NerdStore.WebApp.Tests
 {
+    [TestCaseOrderer("Features.Tests.PriorityOrderer", "Features.Tests")]
     [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class UsuarioTestes
     {
@@ -23,7 +25,7 @@ namespace NerdStore.WebApp.Tests
             _testsFixture = testsFixture;
         }
 
-        [Fact(DisplayName = "Register User")]
+        [Fact(DisplayName = "Register User"), TestPriority(1)]
         [Trait("Category", "Web Integration - User")]
         public async Task User_Register_ShouldRegisterUser()
         {
@@ -35,7 +37,8 @@ namespace NerdStore.WebApp.Tests
             var inicitalResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
             inicitalResponse.EnsureSuccessStatusCode();
 
-            var antiForgeryToken = _testsFixture.ObterAntiForgeryToken(await inicitalResponse.Content.ReadAsStringAsync());
+            var antiForgeryToken =
+                _testsFixture.ObterAntiForgeryToken(await inicitalResponse.Content.ReadAsStringAsync());
 
             var formData = new Dictionary<string, string>
             {
@@ -60,7 +63,84 @@ namespace NerdStore.WebApp.Tests
             postResponse.EnsureSuccessStatusCode();
             Assert.Contains($"Hello {email}", responseString);
         }
+
+
+        [Fact(DisplayName = "Register User Weak Password")]
+        [Trait("Category", "Web Integration - User"), TestPriority(3)]
+        public async Task User_RegisterWeakPass_ShouldNotRegisterUser()
+        {
+            // Arrange
+            _testsFixture.GerarUserSenha();
+            string email = _testsFixture.UsuarioEmail;
+            string pass = "123456"; //weak password
+
+            var inicitalResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Register");
+            inicitalResponse.EnsureSuccessStatusCode();
+
+            var antiForgeryToken =
+                _testsFixture.ObterAntiForgeryToken(await inicitalResponse.Content.ReadAsStringAsync());
+
+            var formData = new Dictionary<string, string>
+            {
+                {_testsFixture.AntiForgeryFieldName, antiForgeryToken},
+                {"Input.Email", email},
+                {"Input.Password", pass},
+                {"Input.ConfirmPassword", pass}
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Register")
+            {
+                Content = new FormUrlEncodedContent(formData)
+
+            };
+
+            // Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+            // Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains($"Passwords must have at least one non alphanumeric character.", responseString);
+            Assert.Contains($"Passwords must have at least one lowercase", responseString);
+            Assert.Contains($"Passwords must have at least one uppercase", responseString);
+        }
+
+
+        [Fact(DisplayName = "Login User"), TestPriority(2)]
+        [Trait("Category", "Web Integration - User")]
+        public async Task User_LoginUser_ShouldLogin()
+        {
+            // Arrange
+            string email = _testsFixture.UsuarioEmail;
+            string pass = _testsFixture.UsuarioSenha;
+
+            var inicitalResponse = await _testsFixture.Client.GetAsync("/Identity/Account/Login");
+            inicitalResponse.EnsureSuccessStatusCode();
+
+            var antiForgeryToken =
+                _testsFixture.ObterAntiForgeryToken(await inicitalResponse.Content.ReadAsStringAsync());
+
+            var formData = new Dictionary<string, string>
+            {
+                {_testsFixture.AntiForgeryFieldName, antiForgeryToken},
+                {"Input.Email", email},
+                {"Input.Password", pass}
+            };
+
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Identity/Account/Login")
+            {
+                Content = new FormUrlEncodedContent(formData)
+
+            };
+
+            // Act
+            var postResponse = await _testsFixture.Client.SendAsync(postRequest);
+
+            // Assert
+            var responseString = await postResponse.Content.ReadAsStringAsync();
+            postResponse.EnsureSuccessStatusCode();
+            Assert.Contains($"Hello {email}", responseString);
+        }
     }
 }
-
-
